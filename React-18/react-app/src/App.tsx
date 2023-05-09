6e5881118221921b0c5ea7724c3c680a75710950
DarcyJHenschke <darcyhenschke@gmail.com>
2023-05-09 22:04:23 +1000

    react 18 study

---
+++ React-18/react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ListGroup from "./components/ListGroup";
+import Message from "./Message";
+
+function App() {
+    return (
+        <div>
+            <ListGroup />
+        </div>
+    );
+}
+
+export default App;
