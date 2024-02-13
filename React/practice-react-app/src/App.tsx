8c11970b64f5ae3ffa12cfb536edf16647c77ed8
DarcyJHenschke <darcyhenschke@gmail.com>
2024-02-13 17:52:34 +1100

    react practice

--- React/practice-react-app/src/App.tsx
+++ React/practice-react-app/src/App.tsx
@@ -1,29 +1,52 @@
 import { useEffect, useRef, useState } from "react";
-import axios from "axios";
-
-interface User {
-    id: number;
-    name: string;
-}
+import axios, { AxiosError, CanceledError } from "axios";
+import userService, { User } from "./services/user-service";
 
 const App = () => {
     const [users, setUsers] = useState<User[]>([]);
     const [error, setError] = useState("");
 
+    const [isLoading, setLoading] = useState(false);
+
     useEffect(() => {
-        const fetchUsers = async () => {
-            const res = await axios.get<User[]>(
-                "https://jsonplaceholder.typicode.com/usersx",
-            );
-            setUsers(res.data);
-        };
+        const { request, cancel } = userService.getAllUsers();
+        request
+            .then((res) => {
+                setUsers(res.data);
+            })
+            .catch((err) => {
+                if (err instanceof CanceledError) return;
+                setError(err.message);
+            });
+
+        return () => cancel();
     }, []);
+
+    const deleteUser = (user: User) => {
+        const originalUsers = [...users];
+        setUsers(users.filter((u) => u.id === user.id));
+
+        userService.deleteUser(user.id).catch((err) => {
+            setError(err.message);
+            setUsers(originalUsers);
+        });
+    };
+
     return (
         <>
+            {isLoading && <p>loading</p>}
             {error && <p className="text-danger">{error}</p>}
             <ul>
                 {users.map((user) => (
-                    <li>{user.name}</li>
+                    <div key={user.id}>
+                        <li>{user.name}</li>
+                        <button
+                            onClick={() => deleteUser(user.id)}
+                            className="btn btn-secondary"
+                        >
+                            Delete
+                        </button>
+                    </div>
                 ))}
             </ul>
         </>
